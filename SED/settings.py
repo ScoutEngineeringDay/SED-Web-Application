ff89e77599515c7e83e6691ee874f1472b37e20d
Ryan Dufrene <rdufrene@mitre.org>
2016-12-22 17:54:48 -0500

    Added the badge with the QR codes that can be downloaded as a pdf (needs to be made dynamic).

--- SED/settings.py
+++ SED/settings.py
@@ -78,28 +78,28 @@ WSGI_APPLICATION = 'SED.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'sed_database_test',
-#         'USER': 'root',
-#         'PASSWORD':'test',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
 DATABASES = {
    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'SED_Database',
-        'USER': 'sed',
-        'PASSWORD':'pass1234',
-        'HOST': 'sed-db.cqpg4rn4jalr.us-east-1.rds.amazonaws.com',
+        'NAME': 'sed_database_test',
+        'USER': 'root',
+        'PASSWORD':'test',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
+# DATABASES = {
+#    'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'SED_Database',
+#         'USER': 'sed',
+#         'PASSWORD':'pass1234',
+#         'HOST': 'sed-db.cqpg4rn4jalr.us-east-1.rds.amazonaws.com',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
