a30c592c0f62a95f396a111dafc6d127794ebfe8
Walter Hiranpat <walterhiranpat92@gmail.com>
2017-03-15 17:09:20 +0000

    update scripts for event

--- SED_RDB/SessionScript.sql
+++ SED_RDB/SessionScript.sql
@@ -4,6 +4,7 @@ CREATE TABLE SED_Database.sedUI_session(
 	scout_id INT(10) ZEROFILL NOT NULL,
 	payment_method ENUM('Pay_Mail', 'Pay_Online', 'Waived') NOT NULL,
 	payment_amount INT UNSIGNED,
+    payment_status ENUM('PAID', 'NOT PAID') NOT NULL,
     open_ceremony CHAR(1),
 	workshop1_id INT(10) ZEROFILL NOT NULL,
     workshop1_status ENUM('COMPLETE', 'INCOMPLETE', 'IN PROGRESS'),
@@ -18,27 +19,27 @@ CREATE TABLE SED_Database.sedUI_session(
 	workshop2_checkout TIMESTAMP DEFAULT NULL);
     
 INSERT INTO SED_Database.sedUI_session
-	(scout_id, session_year, payment_method, payment_amount, open_ceremony, workshop1_id, workshop1_status, workshop2_id, workshop2_status)
+	(scout_id, session_year, payment_method, payment_amount, payment_status, open_ceremony, workshop1_id, workshop1_status, workshop2_id, workshop2_status)
 VALUES
-	(1,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (2,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (3,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (4,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (5,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (6,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (7,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (8,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (9,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (10,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (11,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (12,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (13,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (14,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (15,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (16,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (17,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (18,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (19,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (20,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS'),
-    (21,2017,'Pay_Mail', 40.00,'A', 1,'IN PROGRESS', 2, 'IN PROGRESS');
+	(1,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (2,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (3,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (4,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (5,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (6,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (7,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (8,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (9,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (10,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (11,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (12,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (13,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (14,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (15,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (16,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (17,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (18,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (19,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (20,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS'),
+    (21,2017,'Pay_Mail', 40.00,'PAID', 'A', 1,'IN PROGRESS',5, 'IN PROGRESS');
     
