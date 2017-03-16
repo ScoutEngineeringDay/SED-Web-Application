3b046e0f69938a4aa19593764e6d89150d2f2f32
Walter Hiranpat <walterhiranpat92@gmail.com>
2017-03-16 14:27:33 +0000

    add open ceremony field to admin view

--- sedUI/admin.py
+++ sedUI/admin.py
@@ -22,12 +22,12 @@ class LocationAdmin(admin.ModelAdmin):
 		model = Location
 
 class SessionAdmin(admin.ModelAdmin):
-	list_display = ["session_id","__str__"]
+	list_display = ["session_id","__str__", "open_ceremony"]
 	class Meta:
 		model = Session
 
 class WorkshopAdmin(admin.ModelAdmin):
-	list_display = ["workshop_id","__str__", "__instructor__", "workshop_time", "workshop_open"]
+	list_display = ["workshop_id","__str__", "__instructor__", "workshop_time", "workshop_open", "open_ceremony"]
 	class Meta:
 		model = Workshop
 
