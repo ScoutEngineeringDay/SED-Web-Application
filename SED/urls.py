d4af7dbd6863547dd77a2f64f789d00525b07a85
Walter Hiranpat <tanuguitar@gmail.com>
2017-10-26 20:09:04 -0400

    add statics to url

--- SED/urls.py
+++ SED/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('sedUI.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
