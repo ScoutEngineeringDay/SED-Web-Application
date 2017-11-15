b0bb0371538544c5bfebfd0d1b01a0ddc5ac3b8c
Phillip Marlow <phillip@marlow1.com>
2017-11-14 20:45:28 -0500

    Separate database settings to facilitate Ansible Vault replacements.

---
+++ SED/database_settings.py
@@ -0,0 +1,15 @@
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SED_Database',
+        'MYSQL_USER': 'sed_admin',
+        'USER': 'sed_admin',
+        'PASSWORD':'sed_password',
+        'MYSQL_PASSWORD':'sed_password2',
+        'HOST': '172.17.0.4', #need to change to match container
+        'PORT': '3306',
+    }
+}
