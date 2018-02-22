5e1cdb49c18263af1379f5e9068813663a2c3936
Ryan Dufrene <rdufrene92@gmail.com>
2018-02-22 14:21:25 -0500

    Added functionality of medical and allergy notes being disabled unless the checkbox is checked. Also decreased the textbox limit by 1

--- sedUI/forms.py
+++ sedUI/forms.py
@@ -10,10 +10,10 @@ class RegistrationForm1(forms.Form):
 	citizenship = forms.ChoiceField(widget=forms.RadioSelect(), choices=[('Yes', 'Yes'),('No','No')])
 
 class RegistrationForm2(forms.Form):
-	register_first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name', 'maxlength': '50'}))
-	register_last_name = forms.CharField(widget=forms.TextInput(attrs={'input type': 'text', 'class': 'form-control', 'id': 'last_name', 'name': 'last_name', 'placeholder': 'Last Name', 'maxlength': '50'}))
-	register_email= forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email', 'type': 'email', 'maxlength': '50'}))
-	register_email_confirm = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Confirmation', 'type': 'email', 'maxlength': '50'}))
+	register_first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name', 'maxlength': '49'}))
+	register_last_name = forms.CharField(widget=forms.TextInput(attrs={'input type': 'text', 'class': 'form-control', 'id': 'last_name', 'name': 'last_name', 'placeholder': 'Last Name', 'maxlength': '49'}))
+	register_email= forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email', 'type': 'email', 'maxlength': '49'}))
+	register_email_confirm = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Confirmation', 'type': 'email', 'maxlength': '49'}))
 	register_phone =phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone', 'type': 'tel', 'minlength': '10', 'maxlength': '10'}), required=False)
 	mitre_employee = forms.BooleanField(initial=False, required=False)
 	volunteer_checkbox = forms.BooleanField(initial=False, required=False)
@@ -22,22 +22,22 @@ class RegistrationForm2(forms.Form):
 	# registration_type = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
 
 class RegistrationScoutForm1(forms.Form):
-	first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name', 'maxlength': '50'}))
-	last_name = forms.CharField(widget=forms.TextInput(attrs={'input type': 'text', 'class': 'form-control', 'id': 'last_name', 'name': 'last_name', 'placeholder': 'Last Name', 'maxlength': '50'}))
+	first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name', 'maxlength': '49'}))
+	last_name = forms.CharField(widget=forms.TextInput(attrs={'input type': 'text', 'class': 'form-control', 'id': 'last_name', 'name': 'last_name', 'placeholder': 'Last Name', 'maxlength': '49'}))
 	affiliation = forms.ChoiceField(choices=[("BOY", "Boy Scout"),("GIRL", "Girl Scout"), ("OTHER","Other")], widget=forms.Select(attrs={'class': 'form-control'}))
-	unit_number = forms.IntegerField(min_value=0, max_value=99999999, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Unit Number', 'type': 'number'}))
-	email= forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email', 'type': 'email', 'maxlength': '50'}))
-	email_confirm = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Confirmation', 'type': 'email', 'maxlength': '50'}))
-	phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone', 'type': 'tel', 'minlength': '10', 'maxlength': '10'}), required=False) #TODO make not reqired
+	unit_number = forms.IntegerField(min_value=0, max_value=99999999, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Unit Number', 'type': 'number', 'maxlength': '10'}))
+	email= forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email', 'type': 'email', 'maxlength': '49'}))
+	email_confirm = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Confirmation', 'type': 'email', 'maxlength': '49'}))
+	phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone', 'type': 'tel', 'minlength': '10', 'maxlength': '10'}), required=False)
 
-	emergency_first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Emergency Contact First Name', 'type': 'text', 'maxlength': '50'}))
-	emergency_last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Emergency Contact Last Name', 'type': 'text', 'maxlength': '50'}))
+	emergency_first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Emergency Contact First Name', 'type': 'text', 'maxlength': '49'}))
+	emergency_last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Emergency Contact Last Name', 'type': 'text', 'maxlength': '49'}))
 	emergency_phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Emergency Contact Phone', 'type': 'tel', 'minlength': '10', 'maxlength': '10'}))
-	emergency_email= forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Emergency Email', 'type': 'email', 'maxlength': '50'}))
-	medical_notes = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Medical Notes', 'maxlength': '5000'}), required=False)
-	allergy_notes = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Allergy Notes', 'maxlength': '5000'}), required=False)
-	medical_issues = forms.BooleanField(initial=False, required=False)
-	allergy_issues = forms.BooleanField(initial=False, required=False)
+	emergency_email= forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Emergency Email', 'type': 'email', 'maxlength': '49'}))
+	medical_notes = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Medical Notes', 'maxlength': '4999'}), disabled=True, required=False)
+	allergy_notes = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Allergy Notes', 'maxlength': '4999'}), disabled=True, required=False)
+	medical_issues = forms.BooleanField(widget=forms.CheckboxInput(attrs={'onclick':'medicalChecked()'}), initial=False, required=False)
+	allergy_issues = forms.BooleanField(widget=forms.CheckboxInput(attrs={'onclick':'allergyChecked()'}), initial=False, required=False)
 	photo = forms.BooleanField(initial=True, required=False)
 	# captcha = ReCaptchaField()
 
@@ -62,12 +62,12 @@ class RegistrationPaymentForm(forms.Form):
 	payment_method = forms.ChoiceField(widget=forms.RadioSelect(), choices=[("Pay_Mail","Mail in Check"),("Pay_Online","Online Payment"),("Waived","Waived")])
 
 class ContactEmailForm(forms.Form):
-	contact_first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'First Name', 'maxlength': '50'}))
-	contact_last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Last Name', 'maxlength': '50'}))
-	email_address = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Email', 'size': '40'}))
+	contact_first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'First Name', 'maxlength': '49'}))
+	contact_last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Last Name', 'maxlength': '49'}))
+	email_address = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Email', 'size': '40', 'maxlength': '49'}))
 	CHOICES = (("General Customer Service","General Customer Service"),("Suggestion","Suggestion"),("Product Support","Product Support"),("MITRE Employee", "MITRE Employee Registeration Request"), ("Volunteer","Volunteer Registeration Request"))
 	message_subject = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=CHOICES)
-	message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control','placeholder': 'Message'}))
+	message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control','placeholder': 'Message', 'maxlength': '4999'}))
 	# captcha = ReCaptchaField()
 
 class BadgeForm(forms.Form):
