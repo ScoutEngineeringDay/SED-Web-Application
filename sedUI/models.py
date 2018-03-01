74122ad65dcf1b3127320afd1c79b62461ff3967
Walter Hiranpat <tanuguitar@gmail.com>
2018-02-28 22:14:30 -0500

    fix merge conflict

--- sedUI/models.py
+++ sedUI/models.py
@@ -180,6 +180,8 @@ class AboutPage(models.Model):
 	registrationOpenTime=models.CharField(max_length=5000)
 	locationMap=models.CharField(max_length=5000)
 	forceClosed=models.BooleanField(default=False)
+	parkingLocation=models.CharField(max_length=5000)
+	checkingLocation=models.CharField(max_length=5000)
 
 class HomePage(models.Model):
 	homepage_id=models.AutoField(primary_key=True)
